f3292a81bc9762001eae5511585f350e65a45dce
hdldev123 <hdlgithub@gmail.com>
2025-10-31 10:41:07 -0300

    feat: import do schema do usuario no appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Pagamento, PagamentoSchema } from './pagamento.schema';
+import { Usuario, UsuarioSchema } from './usuario.schema';
 
 @Module({
   imports: [
     //  MongoDB (mesma URL local)
     MongooseModule.forRoot('mongodb://localhost:27017/pagamentos_db_nest'),
     MongooseModule.forFeature([
+      { name: Usuario.name, schema: UsuarioSchema },
       { name: Pagamento.name, schema: PagamentoSchema },
     ]),
   ],
