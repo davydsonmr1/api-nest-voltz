c255b1eb428c66289da61de0f0438d764dfd4797
hdldev123 <hdlgithub@gmail.com>
2025-10-31 10:38:18 -0300

    feat: schema de usuario

---
+++ src/usuario.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Usuario extends Document {
+ 
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  nome: string;
+
+  @Prop({ required: true, unique: true })
+  cpf: string;
+}
+export const UsuarioSchema = SchemaFactory.createForClass(Usuario);
