e86ba584fa1534041d6ac63af1bd94708c9ab00b
hdldev123 <hdlgithub@gmail.com>
2025-11-03 10:09:59 -0300

    feat: documentação

--- src/main.ts
+++ src/main.ts
@@ -7,22 +7,20 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // 3. Diz ao Nest para TAMBÉM se conectar ao RabbitMQ como um "ouvinte"
+  
   app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ, // Define o "transporte" como RabbitMQ
+    transport: Transport.RMQ, 
     options: {
-      urls: ['amqp://localhost:5672'], // A URL do nosso Rabbit no Docker
-      queue: 'fila_de_atualizacoes', // O NOME da fila que ele vai "vigiar"
+      urls: ['amqp://localhost:5672'], 
+      queue: 'fila_de_atualizacoes', 
       queueOptions: {
-        durable: true, // Garante que a fila sobreviva se o Rabbit reiniciar
+        durable: true, 
       },
     },
   });
-
-  // 4. Inicia TODOS os microserviços
+  
   await app.startAllMicroservices();
   
-  // 5. Inicia a API Web 
   await app.listen(3000);
   console.log('API Web rodando em http://localhost:3000');
   console.log('RabbitMQ vigiando a "fila_de_atualizacoes"');
